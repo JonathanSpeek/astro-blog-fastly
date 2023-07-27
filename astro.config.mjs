43f71558690641fecea1e8fdde4a42aca62ba886
Jonathan Speek <speek@fastmail.com>
2023-07-27 14:24:40 -0600

    Fix base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://jonathanspeek.github.io",
-  base: "/astro-blog-fastly",
+  base: "/",
   integrations: [mdx(), sitemap()],
 });
